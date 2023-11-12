3fcdce4fa609be3413620063218ac5bd8cf3d12c
Rafael Lenti <rafael.lenti@gmail.com>
2023-11-12 14:40:26 -0300

    add method logic to routing navigation service

--- client/src/app/services/routingNavigation.ts
+++ client/src/app/services/routingNavigation.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Bank } from "../interfaces/bankInterface";
 import { Router } from "@angular/router";
+import { RoutesEnum } from "../enum/routesEnum";
 
 @Injectable({
     providedIn: 'root'
@@ -9,7 +10,8 @@ import { Router } from "@angular/router";
 export class RoutingNavigation {
     constructor(private router: Router) { }
 
-    navigateToAccountRegistration(bank?: Bank): void {
-
+    navigateToRoute(route: RoutesEnum, bank?: Bank): void {
+        const url = bank ? [route, bank.code] : [route];
+        this.router.navigate(url);
     }
 }
