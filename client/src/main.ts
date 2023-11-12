203e47b40f66804df50afa140c7b1092705a10f4
Rafael Lenti <rafael.lenti@gmail.com>
2023-11-11 22:36:43 -0300

    resolve main ts conflict

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-<<<<<<< Updated upstream
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-=======
 import { provideRouter } from '@angular/router';
 import routeConfig from './app/app.routes';
 import { HttpClientModule } from '@angular/common/http';
@@ -17,4 +12,3 @@ bootstrapApplication(AppComponent, {
     importProvidersFrom(HttpClientModule)
   ]
 }).catch(err => console.error(err));
->>>>>>> Stashed changes
