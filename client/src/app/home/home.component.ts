46bf57d97a5234d6bffad8555d760643b811a736
Rafael Lenti <rafael.lenti@gmail.com>
2023-11-12 14:45:50 -0300

    import routing navigation service

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BankAccount } from '../interfaces/bankAccountInterface';
 import { BankAccountService } from '../services/bankAccountService';
+import { RoutingNavigation } from '../services/routingNavigation';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,7 @@ import { BankAccountService } from '../services/bankAccountService';
 })
 
 export class HomeComponent implements OnInit {
-  constructor(private bankAccountService: BankAccountService) { }
+  constructor(private bankAccountService: BankAccountService, public routingNavigationService: RoutingNavigation) { }
 
   public bankAccountList: BankAccount[] = [];
 
