80a2fa7ae69e16e4243790e2002aa83e2334cefa
Rafael Lenti <rafael.lenti@gmail.com>
2023-11-11 21:13:45 -0300

    create bank list component

---
+++ client/src/app/banks-list/banks-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-banks-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './banks-list.component.html',
+  styleUrl: './banks-list.component.scss'
+})
+
+export class BanksListComponent {
+
+}
