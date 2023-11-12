e5f5b7a5a407b28b7fd32f07af205efdddc73de2
Rafael Lenti <rafael.lenti@gmail.com>
2023-11-11 21:21:35 -0300

    feature: fetch banks list

--- client/src/app/banks-list/banks-list.component.ts
+++ client/src/app/banks-list/banks-list.component.ts
@@ -10,6 +10,28 @@ import { CommonModule } from '@angular/common';
   styleUrl: './banks-list.component.scss'
 })
 
-export class BanksListComponent {
+export class BanksListComponent implements OnInit, OnDestroy {
+  public banksList: Bank[] = [];
+  subscription: Subscription | undefined;
 
-}
+  constructor(private bankService: BankService, private router: Router) { }
+
+  ngOnInit() {
+    this.getBanksList();
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
+
+  getBanksList(): void {
+    this.subscription = this.bankService.getBanksList().subscribe({
+      next: (response) => {
+        this.banksList = response;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+} 
