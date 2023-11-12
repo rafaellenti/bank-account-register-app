4ba4c769dc74f82d976052c183d26872b9647f2c
Rafael Lenti <rafael.lenti@gmail.com>
2023-11-11 22:29:33 -0300

    setting routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,12 +1,29 @@
 import { Routes } from '@angular/router';
+<<<<<<< Updated upstream
+
+export const routes: Routes = [];
+=======
 import { HomeComponent } from './home/home.component';
+import { BanksListComponent } from './banks-list/banks-list.component';
+import { AccountRegisterComponent } from './account-register/account-register.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
         title: 'Home'
+    },
+    {
+        path: 'banks-list',
+        component: BanksListComponent,
+        title: 'Banks List'
+    },
+    {
+        path: 'register-account/:id',
+        component: AccountRegisterComponent,
+        title: 'Register Account'
     }
 ];
 
 export default routes;
+>>>>>>> Stashed changes
