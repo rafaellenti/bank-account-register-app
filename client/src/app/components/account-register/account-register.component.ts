dc5073f38b1a0f4722075bedca5137056077ecbb
Rafael Lenti <rafael.lenti@gmail.com>
2023-11-12 21:30:26 -0300

    add fetch bank data from api in account register component

--- client/src/app/components/account-register/account-register.component.ts
+++ client/src/app/components/account-register/account-register.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BanksListComponent } from '../banks-list/banks-list.component';
+import { Bank } from '../../interfaces/bankInterface';
+import { BankService } from '../../services/bankService';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-account-register',
@@ -11,9 +15,36 @@ import { BanksListComponent } from '../banks-list/banks-list.component';
 })
 
 export class AccountRegisterComponent implements OnInit {
-  public banksListComponent = BanksListComponent;
+  subscription: Subscription | undefined;
+  private codeId: string | null = null;
+  public bank: Bank = {
+    ispb: 0,
+    name: '',
+    code: 0,
+    fullName: ''
+  };
+
+  constructor(private bankService: BankService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getBank();
+  }
+
+  getBank(): void {
+    this.codeId = this.route.snapshot.paramMap.get('id');
+
+    if (this.codeId) {
+      const code = parseInt(this.codeId, 10)
+
+      this.subscription = this.bankService.getBank(code).subscribe({
+        next: (response) => {
+          this.bank = response;
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      })
+    }
 
   }
 }
